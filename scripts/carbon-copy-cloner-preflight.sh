c5ca41330da47553396483f6550d10539c882094
Jarrod Davis <developer@jarrodldavis.com>
2019-07-19 22:44:41 -0600

    carbon-copy-cloner: update preflight script
    Update preflight script for Carbon Copy Cloner to include public and
    private User Defaults exports.

--- scripts/carbon-copy-cloner-preflight.sh
+++ scripts/carbon-copy-cloner-preflight.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
 
-sudo -u jarrodldavis -- bash -l -c "reattach-to-user-namespace update-dotbot-lists"
-exit $?
+function __run_command() {
+  sudo -u jarrodldavis -- bash -l -c "reattach-to-user-namespace $*" || exit $?
+}
+
+__run_command update-dotbot-lists
+__run_command export-user-defaults
+__run_command export-user-defaults --private
