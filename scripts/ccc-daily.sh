c6a48f76794850ab3288f6132ae37e00d17ba308
Jarrod Davis <developer@jarrodldavis.com>
2019-08-15 15:40:18 -0600

    dotbot-update: rename
    - rename `update-dotbot-lists` to `dotbot-update`

--- scripts/ccc-daily.sh
+++ scripts/ccc-daily.sh
@@ -6,7 +6,7 @@ function __run_command() {
   sudo -u jarrodldavis -- bash -l -c "reattach-to-user-namespace $*" || exit $?
 }
 
-__run_command update-dotbot-lists
+__run_command dotbot-update
 __run_command export-user-defaults
 __run_command export-user-defaults --private
 __run_command topgrade --cleanup --no-retry
