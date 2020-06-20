f3bf1f3a5aff560d5bc38108f98290cce3c4c101
Jarrod Davis <developer@jarrodldavis.com>
2020-06-20 05:56:56 -0600

    vscode: improvements to devcontainer dotfiles
    - vscode: generalize GitHub installation method
    - vscode: install `bat` and `hub` from GitHub
    - vscode: force ZSH as the default shell even with container-specific
       Bash override

--- scripts/vscode-devcontainer.sh
+++ scripts/vscode-devcontainer.sh
@@ -28,24 +28,51 @@ rm /tmp/common-setup.sh
 ## Install Live Share prerequisites
 curl -fsSL https://raw.githubusercontent.com/MicrosoftDocs/live-share/master/scripts/linux-prereqs.sh | bash
 
-# Install hub
-apt-get -y install hub
-
 # Install starship prompt
 curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
 
-# Install diff-so-fancy
-mkdir -p /opt/diff-so-fancy/
-curl -fsSL https://github.com/so-fancy/diff-so-fancy/archive/v1.3.0.tar.gz | tar -xz -C /opt/diff-so-fancy --strip-components=1
-ln -sf /opt/diff-so-fancy/diff-so-fancy /usr/local/bin
+# Install additional tools
+function install_from_github() {
+    local OWNER="$1"
+    local REPO="$2"
+    local FILE="$3"
+    local BIN_REL="$4"
+
+    local RELEASE_URL="https://api.github.com/repos/$OWNER/$REPO/releases/latest"
+
+    case "$FILE" in
+        "__TARBALL__")
+            local JQ_SCRIPT=".tarball_url"
+            ;;
+        *)
+            local JQ_SCRIPT=".assets[].browser_download_url | select(. | contains(\"$FILE\"))"
+            ;;
+    esac
+
+    local DOWNLOAD_URL="$(curl -s $RELEASE_URL | jq -r "$JQ_SCRIPT")"
+
+    local INSTALL_DIR="/opt/github.com/$OWNER/$REPO/"
+    mkdir -p "$INSTALL_DIR"
+    curl -fsSL $DOWNLOAD_URL | tar -xz -C "$INSTALL_DIR" --strip-components=1
+
+    local BIN_ABS="$INSTALL_DIR/$BIN_REL"
+    ln -s "$BIN_ABS" /usr/local/bin
+}
+
+install_from_github "sharkdp" "bat" "-x86_64-unknown-linux-gnu.tar.gz" "bat"
+install_from_github "github" "hub" "hub-linux-amd64" "bin/hub"
+install_from_github "so-fancy" "diff-so-fancy" "__TARBALL__" "diff-so-fancy"
+
+# Force ZSH as default shell
+echo "exec zsh" > ~/.bashrc
 
 # Copy configs
 cp ~/.dotfiles/zshrc ~/.zshrc
 cp ~/.dotfiles/zprofile ~/.zprofile
 cp ~/.dotfiles/starship.toml ~/.starship.toml
-sed -i "s/export EDITOR='vim'/export EDITOR='code --wait'/" ~/.zshrc
 
 # Set EDITOR to Visual Studio Code
+sed -i "s/export EDITOR='vim'/export EDITOR='code --wait'/" ~/.zshrc
 
 # Clean up
 apt-get autoremove -y
