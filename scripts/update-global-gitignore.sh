b577d467a7f944838b5ccdbb2009e0ddcf63d211
Jarrod Davis <developer@jarrodldavis.com>
2024-11-20 15:05:48 -0700

    gitignore: update

--- scripts/update-global-gitignore.sh
+++ scripts/update-global-gitignore.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 set -euo pipefail
 
-gibo update
+gibo-update
 GITIGNORE_FILE="$HOME/.dotfiles/configs/gitignore"
 GITIGNORE_CONTENTS="$(cat "$GITIGNORE_FILE")"
 echo "$GITIGNORE_CONTENTS" | "$HOME/.dotfiles/scripts/gibo-redump.sh" > "$GITIGNORE_FILE"
