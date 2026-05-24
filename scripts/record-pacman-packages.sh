a74f20296b04780eed4047f9f9ffe83bf9cfe1ce
Jarrod Davis <developer@jarrodldavis.com>
2026-05-23 23:31:28 -0600

    pacman: include bootloader defaults

--- scripts/record-pacman-packages.sh
+++ scripts/record-pacman-packages.sh
@@ -5,19 +5,22 @@ cd ~/.dotfiles/configs/pacman
 
 cachyos_calamares_sha="0bf08362ce5ccafe3d8cfc929a9eed801e05e8f8"
 base_packages_url="https://raw.githubusercontent.com/cachyos/cachyos-calamares/${cachyos_calamares_sha}/src/modules/pacstrap/pacstrap.conf"
+bootloader_packages_url="https://raw.githubusercontent.com/cachyos/cachyos-calamares/${cachyos_calamares_sha}/src/modules/pacstrap/main.py"
 default_packages_url="https://raw.githubusercontent.com/cachyos/cachyos-calamares/${cachyos_calamares_sha}/src/modules/netinstall/netinstall.yaml"
 
 base_packages="$(curl -fsSL "$base_packages_url" | yq -r '.basePackages[]')"
+bootloader_packages="$(curl -fsSL "$bootloader_packages_url" | grep -E '^\s+base_packages \+=' | grep -oE '"[^"]+"' | tr -d '"')"
 default_packages="$(curl -fsSL "$default_packages_url" | yq -r '.. | objects | select(.selected == true) | .packages[]?')"
 kde_packages="$(curl -fsSL "$default_packages_url" | yq -r '.. | objects | select(.name == "KDE-Desktop") | .packages[]?')"
 chwd_packages="$(cat /var/lib/chwd/local/**/profiles.toml | grep '^packages =' | cut -d'"' -f2 | xargs | tr ' ' '\n')"
 
-echo "$base_packages"    > defaults-base.txt
+echo "$base_packages" > defaults-base.txt
+echo "$bootloader_packages" > defaults-bootloader.txt
 echo "$default_packages" > defaults-netinstall.txt
-echo "$kde_packages"     > defaults-kde.txt
-echo "$chwd_packages"    > defaults-chwd.txt
+echo "$kde_packages" > defaults-kde.txt
+echo "$chwd_packages" > defaults-chwd.txt
 
-cat defaults-{base,netinstall,kde,chwd}.txt | sort | uniq > defaults-all.txt
+cat defaults-{base,bootloader,netinstall,kde,chwd}.txt | sort | uniq > defaults-all.txt
 
 pacman -Qeq | sort > installed-all.txt
 
