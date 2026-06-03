e5f0eb4f2cf7ece6a39da0e542b79469b5264c45
Jarrod Davis <developer@jarrodldavis.com>
2026-06-03 15:21:44 -0600

    install: improve local dev container testing

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 echo '==> Fixing directory permissions...'
 sudo chown -v "$(id -un)":"$(id -gn)" .. . .git
 
+echo '==> Adding additional git safe directory...'
+git config --global --add safe.directory ~/.dotfiles
+
 echo '==> Done!'
