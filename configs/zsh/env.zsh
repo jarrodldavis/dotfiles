473952c3352078e434853bb86afef9b58f10f984
Jarrod Davis <developer@jarrodldavis.com>
2026-07-06 05:33:15 -0600

    zsh: add `codex-resets` function

--- configs/zsh/env.zsh
+++ configs/zsh/env.zsh
@@ -1,5 +1,6 @@
 # shellcheck disable=SC2206
 fpath=( ~/.dotfiles/configs/zshfuncs $fpath )
 autoload -Uz backblaze-upgrade
+autoload -Uz codex-resets
 autoload -Uz docker-reset
 autoload -Uz fix-smb-permissions
