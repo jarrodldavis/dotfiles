98d97184dfee10774f556c1e60f0cdc36f257f56
Jarrod Davis <developer@jarrodldavis.com>
2026-07-06 09:41:56 -0600

    zsh: use gh-cd

--- configs/zsh/rc.zsh
+++ configs/zsh/rc.zsh
@@ -12,3 +12,9 @@ unsetopt NOMATCH
 if [ -z "${EDITOR:-}" ]; then
     export EDITOR="vim"
 fi
+
+if command -v gh >/dev/null 2>&1; then
+    if gh extension list | grep -q 'gh cd'; then
+        eval "$(gh extension exec cd init zsh --wrap-gh)"
+    fi
+fi
