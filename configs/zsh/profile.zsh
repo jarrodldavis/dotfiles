de6be4c75bca5c758941dd46e5d0b6d968bde778
Jarrod Davis <developer@jarrodldavis.com>
2026-09-13 09:19:10 -0600

    zsh: unify check for brew binary

--- configs/zsh/profile.zsh
+++ configs/zsh/profile.zsh
@@ -1,7 +1,7 @@
-if [ -f "/opt/homebrew/bin/brew" ]; then
+if [ -x "/opt/homebrew/bin/brew" ]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-if [ -f "/usr/local/bin/brew" ]; then
+if [ -x "/usr/local/bin/brew" ]; then
     eval "$(/usr/local/bin/brew shellenv)"
 fi
